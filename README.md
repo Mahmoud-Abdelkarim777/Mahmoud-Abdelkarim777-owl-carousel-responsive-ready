e381142389da187e0b7085abcbccc13915aafced
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2024-11-03 01:47:06 +0200

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+# owl-carousel-responsive-ready
+## https://mahmoud-abdelkarim777.github.io/owl-carousel-responsive-ready/
+# Mahmoud-Abdelkarim777-owl-carousel-responsive-ready
---
+++ main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+})
